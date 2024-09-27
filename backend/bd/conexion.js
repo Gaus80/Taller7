1e40bb993090a3431125ed1d6fe1076fc3144276
Stiven <usgarzon@ucundinamarca.edu.co>
2024-09-27 18:41:37 -0500

    conexion

---
+++ backend/bd/conexion.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+const db = mysql.createConnection ({
+    host: "localhost",//"192.168.0.4",//"192.168.20.132", //"201.221.172.25",
+    user: "root",//"jpgarzon",
+    password: "",///SUS CONTRASEÑAS
+    database: "sistema_asistencia",
+});
+
+db.connect((err)=> {
+    if (err) {
+        throw err;
+    }
+    console.log ("BD Mysql Conectado");
+});
+
+module.exports = db;
