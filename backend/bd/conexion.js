edbc6ca5ac5d06288fc8582f5f9c72c91fb89176
Stiven <usgarzon@ucundinamarca.edu.co>
2024-09-28 17:19:18 -0500

    Subir conexion

--- backend/bd/conexion.js
+++ backend/bd/conexion.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection ({
     host: "127.0.0.1",//"192.168.0.4",//"192.168.20.132", //"201.221.172.25",
     user: "root",//"jpgarzon",
-    password: "Gaus8021.",///SUS CONTRASEÑAS
+    password: "",///SUS CONTRASEÑAS
     database: "sistema_asistencia",
 });
 
